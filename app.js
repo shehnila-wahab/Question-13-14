b820136f74c5869466d558350c0e4accff6db179
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 22:51:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var transporatation = ['civic', 'honda', 'bus', 'car',];
+for (var i = 0; i < transporatation.length; i++) {
+    console.log('i would like to own a' + transporatation(i));
+}
+// class 14
+var guest_list = ['Ali', 'aman', 'ayan', 'sara', 'sanam'];
+for (var i_1 = 0; i_1 < guest_list.length; i_1++) {
+    console.log('respected sir/madam' + guest_list(i_1) + ',\n', we, invited, you, on, dinner, tomorrow., n, n, thank, you, '));
+}
---
+++ app.ts
@@ -0,0 +1,31 @@
+var transporatation : string[] =['civic','honda','bus','car',];
+for(var i=0; i<transporatation.length; i++){
+    console.log('i would like to own a' + transporatation[i]);
+}
+
+
+// class 14
+let guest_list : string[] =['Ali','aman','ayan','sara','sanam'];
+for (let i=0; i<guest_list.length; i++){
+    console.log(`respected sir/madam' + guest_list[i] + ',\n'we invited you on dinner tomorrow.\n\n thank you`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
